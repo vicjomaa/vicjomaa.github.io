e41312c92fcb8058a591c810087af18dc64e48e9
vicjomaa <vjmahechaa@gmail.com>
2024-03-23 21:48:19 +0100

    Initial main

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import glsl from 'vite-plugin-glsl'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), glsl()],
+  plugins: [svelte()],
   build: {
     outDir: './docs', // relative to index.html
     // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
